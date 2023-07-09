fbcbdb03998ef354fe453450a60c27b25c246861
Sunil Kumar Ketha <sunilkumarketha@gmail.com>
2023-07-09 12:37:09 +0200

    Update Sample.java

--- selenium/src/test/java/selenium/Sample.java
+++ selenium/src/test/java/selenium/Sample.java
@@ -2,13 +2,14 @@ package selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Sample {
     @Test
     public void FirstProgram() throws InterruptedException {
         System.out.println("Program Started!!!");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         Thread.sleep(3000);
