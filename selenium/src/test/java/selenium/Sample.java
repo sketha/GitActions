08502d39c33ff3e5b5a36bfac8e6b55d89f5ff78
Sunil Kumar Ketha <sunilkumarketha@gmail.com>
2023-07-09 12:47:06 +0200

    Update Sample.java

--- selenium/src/test/java/selenium/Sample.java
+++ selenium/src/test/java/selenium/Sample.java
@@ -9,7 +9,12 @@ public class Sample {
     @Test
     public void FirstProgram() throws InterruptedException {
         System.out.println("Program Started!!!");
-        WebDriver driver = new FirefoxDriver();
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(firefoxBinary);
+        options.setHeadless(true);  // <-- headless set here
+        WebDriver driver = new FirefoxDriver(options);
+       // WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         Thread.sleep(3000);
