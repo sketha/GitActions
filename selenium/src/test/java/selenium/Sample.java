34644efc123d7edae597e343d66ca6e0bef661dd
Sunil Kumar Ketha <sunilkumarketha@gmail.com>
2023-07-09 12:47:39 +0200

    Update Sample.java

--- selenium/src/test/java/selenium/Sample.java
+++ selenium/src/test/java/selenium/Sample.java
@@ -3,6 +3,9 @@ package selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 
 public class Sample {
